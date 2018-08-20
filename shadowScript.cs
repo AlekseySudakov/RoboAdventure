a77f00959fec69dbdaaf63aa20f564921ee343b1
RenegatDV <renegatdv@live.com>
2018-08-20 20:46:00 +1000

    20.08

--- shadowScript.cs
+++ shadowScript.cs
@@ -1,16 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class shadowScript : MonoBehaviour {
 	public GameObject player;
+	public Scene scene;
+	public float yValue;
 	// Use this for initialization
 	void Start () {
+		scene = SceneManager.GetActiveScene();
+		if (scene.name == "demoScene"){
+			//transform.position = new Vector3(player.transform.position.x,-4.508f,0);
+			yValue = -4.506f;
+		}
+		else if (scene.name == "city"){
+			yValue = -3.87f;
+		}
 		player = GameObject.Find("player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = new Vector3(player.transform.position.x,-4.508f,0);
+			transform.position = new Vector2(player.transform.position.x, yValue);//-4.506f);
+		
 	}
 }
